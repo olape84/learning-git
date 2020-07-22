47ea319bc346297a3383001bde46b133f18f160f
Aleksandra Przywrzej <przepjurka@gmail.com>
2020-07-22 18:58:06 +0200

    niedzialajaca czesc sklepu - warzywniak - buraki

---
+++ sklep.py
@@ -0,0 +1,4 @@
+shopping_list = {'Piekarnia' :  ['Chleb', 'Pączek', 'Bułki'], \
+'Warzywniak' :  ['Marchew', 'Seler', 'Rukola']}
+for  sklep in shopping_list:
+    print('Idę do  {sklep} kupic następujące {shopping_list.values} '(sklep, shopping_list.values))
