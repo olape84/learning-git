d5d7a45d977796864c46ef0e02e566003c7399a8
Aleksandra Przywrzej <przepjurka@gmail.com>
2020-07-22 19:05:07 +0200

    naprawiony plik listy zakupow

--- sklep.py
+++ sklep.py
@@ -1,4 +1,4 @@
 shopping_list = {'Piekarnia' :  ['Chleb', 'Pączek', 'Bułki'], \
 'Warzywniak' :  ['Marchew', 'Seler', 'Rukola']}
 for  sklep in shopping_list:
-    print('Idę do  {sklep} kupic następujące {shopping_list.values} '(sklep, shopping_list.values))
+    print(f'Idę do  {sklep} kupic następujące {shopping_list[sklep]} ')
